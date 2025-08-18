7bd58665453237e67d1b75db7a5dccb980857df4
Jawad Hussain <jawadhussain968@gmail.com>
2025-08-18 15:13:43 -0400

    Bootstrap and dynamic data

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'location': 'New York',
+        'description': 'Develop and maintain web applications.',
+        'date_posted': '2023-10-01'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'San Francisco',
+        'description': 'Analyze and interpret complex data.',
+        'date_posted': '2023-09-15'
+    },
+    {
+        'id': 3,
+        'title': 'Product Manager',
+        'location': 'Remote',
+        'description': 'Lead product development and strategy.',
+        'date_posted': '2023-08-20'
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
